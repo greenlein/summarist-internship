bd265e125803cc88f6f2d85f8ba7202a9f159f8d
Sebastian Giraldo <sebastiangiraldo96@gmail.com>
2026-09-13 14:51:01 -0500

    fix: settings 'Manage Subscription' loading state

--- src/components/SettingsTab.tsx
+++ src/components/SettingsTab.tsx
@@ -4,15 +4,24 @@ import type { RootState } from "../redux/store";
 import { useNavigate } from "react-router";
 import app from "../firebase";
 import { getPortalUrl } from "../stripe/stripePayments";
+import { useEffect, useState } from "react";
+import { FaSpinner } from "react-icons/fa";
 
 export default function Settings() {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
   const user = useSelector((state: RootState) => state.auth);
 
   const manageSubscription = async () => {
-    const portalUrl = await getPortalUrl(app);
-    window.open(portalUrl);
+    try {
+      const portalUrl = await getPortalUrl(app);
+      window.open(portalUrl);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  useEffect(() => {}, []);
 
   return (
     <div className="settings--container">
@@ -41,14 +50,25 @@ export default function Settings() {
       {user.subscription === "premium" && (
         <>
           <div className="settings__content">
-            <button className="settings__btn btn" onClick={() => manageSubscription()}>
-              Manage Subscription
+            <button
+              className="settings__btn btn"
+              onClick={() => {
+                manageSubscription();
+                setIsLoading(true);
+              }}
+            >
+              {isLoading ? <FaSpinner className="settings__spinner" /> : "Manage Subscription"}
             </button>
           </div>
 
           {user.email === "guest@email.com" && (
             <div className="settings__content">
-              <button className="settings__btn btn" onClick={() => navigate("/choose-plan")}>
+              <button
+                className="settings__btn btn"
+                onClick={() => {
+                  navigate("/choose-plan");
+                }}
+              >
                 See All Plans
               </button>
             </div>
